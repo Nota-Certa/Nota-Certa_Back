cde92e52e3446c113db2e5bc87e0149194a03af1
Stella Nazário <stellanacleto@gmail.com>
2025-06-24 20:16:28 -0300

    SCRUM-42 Alteração do controller de Dashboard para comunicação via mensagens

--- apps/dashboard/src/dashboard.controller.ts
+++ apps/dashboard/src/dashboard.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
-  @Get()
-  getHello(): string {
-    return this.dashboardService.getHello();
+  @MessagePattern('dashboard.notas-por-ano')
+  getNotasPorAno() {
+    return this.dashboardService.getNotasPorAno();
+  }
+
+  @MessagePattern('dashboard.ranking-clientes')
+  getRankingClientes() {
+    return this.dashboardService.getRankingClientes();
+  }
+
+  @MessagePattern('dashboard.notas-por-mes')
+  getNotasPorMes() {
+    return this.dashboardService.getNotasPorMes();
   }
 }
--- apps/dashboard/src/main.ts
+++ apps/dashboard/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { DashboardModule } from './dashboard.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(DashboardModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.createMicroservice(DashboardModule, {
+    transport: Transport.REDIS,
+    options: {
+      url: 'redis://localhost:6379'},
+  });
+  await app.listen();
 }
 bootstrap();
