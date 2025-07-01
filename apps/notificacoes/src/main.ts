7cba5784456046c3bb470dc5d5a02a2e03fa2f93
Tom-Jones <tom.jones.silva@gmail.com>
2025-07-01 18:26:32 -0300

    SCRUM-43 Alteração do controller de Notificações para comunicação via mensagens

--- apps/notificacoes/src/main.ts
+++ apps/notificacoes/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificacoesModule } from './notificacoes.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificacoesModule);
-  await app.listen(process.env.PORT ?? 3003);
+  const app = await NestFactory.createMicroservice(NotificacoesModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: process.env.REDIS_HOST || 'redis',
+      port: parseInt(process.env.REDIS_PORT || '6380'),
+    },
+  });
+  await app.listen();
 }
 bootstrap();
