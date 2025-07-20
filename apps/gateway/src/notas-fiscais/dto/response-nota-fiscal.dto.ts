b476ff219cc0e744669a825294724e7d98829344
Dayanne Carolina <daycarol848@gmail.com>
2025-07-20 19:22:51 -0300

    SCRUM-46 Criação do gateway para Notas Fiscais - Atualização

--- apps/gateway/src/notas-fiscais/dto/response-nota-fiscal.dto.ts
+++ apps/gateway/src/notas-fiscais/dto/response-nota-fiscal.dto.ts
@@ -1,79 +1,54 @@
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsString,
-  Length,
-  IsDateString,
-  ValidateNested,
-  IsArray, ArrayMinSize,
-  IsOptional,
-  IsUUID
-} from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
+import { TipoPessoa } from "../enums/tipo-pessoa.enum";
+import { StatusNotaFiscal } from "../enums/status.enum";
+import { CreateNotaFiscalItemDto } from "./create-nota-fiscal-item.dto";
 
-import { Type } from 'class-transformer';
-import { TipoPessoa } from '../enums/tipo-pessoa.enum';
-import { StatusNotaFiscal } from '../enums/status.enum';
-import { CreateNotaFiscalItemDto } from './create-nota-fiscal-item.dto';
-import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateNotaFiscalDto {
-  @IsUUID()
+export class ResponseNotaFiscalDto {
   @ApiProperty({
     description: 'ID da empresa associada à nota fiscal',
     example: '123e4567-e89b-12d3-a456-426614174000',
   })
   empresa_id: string;
 
-  @IsEnum(TipoPessoa)
   @ApiProperty({
     description: 'Tipo de pessoa que gerou a nota fiscal',
     example: TipoPessoa.FISICA,
   })
   tipo_pessoa: TipoPessoa;
 
-  // aceita 11 ou 14 dígitos
-  @IsString()
-  @Length(11, 14)
+
   @ApiProperty({
     description: 'Documento da pessoa',
     example: '12312312312',
   })
   documento: string;
 
-  @IsString()
-  @IsNotEmpty()
    @ApiProperty({
-    description: 'Nome ou razão social da empresa',
+    description: 'Razão social da empresa',
     example: 'EMPRESA X',
   })
   nome_razao_social: string;
 
-  @IsOptional()
-  @IsEnum(StatusNotaFiscal)
+
   @ApiProperty({ 
     description: 'Status da nota fiscal', 
     example: StatusNotaFiscal.PAGA 
   })
-  status?: StatusNotaFiscal;
+  status: StatusNotaFiscal;
 
-  @IsDateString()
   @ApiProperty({
     description: 'Data de emissão da assinatura',
     example: '2025-07-20T12:30:00.000Z',
   })
   data_emissao: string;
 
-  @IsDateString()
   @ApiProperty({
     description: 'Data de vencimento da assinatura',
     example: '2025-07-20T12:30:00.000Z',
   })
   data_vencimento: string;
 
-  @IsArray()
-  @ArrayMinSize(1)
-  @ValidateNested({ each: true })
-  @Type(() => CreateNotaFiscalItemDto)
   @ApiProperty({
     description: 'Itens',
     example: '[{}]',
--- apps/gateway/src/notas-fiscais/notas-fiscais.gateway.ts
+++ apps/gateway/src/notas-fiscais/notas-fiscais.gateway.ts
@@ -12,28 +12,66 @@ import { NotasFiscaisService } from './notas-fiscais.service';
 import { UpdateNotaFiscalDto } from './dto/update-nota-fiscal.dto';
 import { CreateNotaFiscalDto } from 'apps/notas-fiscais/src/dto/create-nota-fiscal.dto';
 import { ExportNotaFiscalFormat } from './enums/export-nota-fiscal-format.enum';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ResponseNotaFiscalDto } from './dto/response-nota-fiscal.dto';
 
+@ApiTags('Notas-Fiscais')
 @Controller('notas-fiscais')
 export class NotasFiscaisController {
   constructor(private readonly notasFiscaisService: NotasFiscaisService) {}
 
   // Rotas para Assinaturas
   @Get('notas')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Lista de todas as Notas fiscais' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de notas fiscais retornada com sucesso.',
+  })
+  @ApiResponse({ status: 404, description: 'Nenhuma nota fiscal encontrada.' })
   getAllNotas() {
     return this.notasFiscaisService.buscarTodasNotas();
   }
 
   @Get('notas/:id')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Busca de nota fiscal por ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Nota fiscal encontrada com sucesso.',
+  })
+  @ApiResponse({ status: 400, description: 'ID inválido.' })
+  @ApiResponse({ status: 404, description: 'Nota fiscal não encontrada.' })
   getNotaPorId(@Param('id') id: string) {
     return this.notasFiscaisService.buscarNotasPorId(id);
   }
 
   @Post('notas')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Cria uma nova nota fiscal' })
+  @ApiResponse({
+    status: 201,
+    description: 'Nota fiscal criada com sucesso.',
+    type: CreateNotaFiscalDto,
+  })
+  @ApiResponse({ status: 400, description: 'Dados inválidos.' })
+  @ApiResponse({ status: 409, description: 'Assinatura já existe.'})
+  @ApiBody({ type: CreateNotaFiscalDto })
   criarNota(@Body() dto: CreateNotaFiscalDto) {
     return this.notasFiscaisService.criarNota(dto);
   }
 
   @Put('notas/:id')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Atualiza uma nota fiscal pelo ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Nota fiscal atualizada com sucesso.',
+    type: ResponseNotaFiscalDto,
+  })
+  @ApiResponse({ status: 400, description: 'Dados inválidos.' })
+  @ApiResponse({ status: 404, description: 'Nota fiscal não encontrada.' })
+  @ApiBody({ type: UpdateNotaFiscalDto })
   atualizarNota(
     @Param('id') id: string,
     @Body() dto: UpdateNotaFiscalDto,
@@ -42,32 +80,74 @@ export class NotasFiscaisController {
   }
 
   @Delete('notas/:id')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Remove uma nota fiscal pelo ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Nota fiscal removida com sucesso.',
+  })
+  @ApiResponse({ status: 404, description: 'Nota fiscal não encontrada.' })
   deletarNota(@Param('id') id: string) {
     return this.notasFiscaisService.deletarNota(id);
   }
 
   // Rotas para Planos
   @Get('notas/periodo')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Lista as notas fiscais de um período' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de notas fiscais retornada com sucesso.',
+  })
+  @ApiResponse({ status: 404, description: 'Nenhuma nota encontrado.' })
   getAllPlanos(@Param('mes') mes: number | undefined, @Param('ano') ano: number) {
     return this.notasFiscaisService.buscarNotasPorPeriodo(mes, ano);
   }
 
   @Get('notas/ranking')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Lista as notas fiscais por ranking' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de notas fiscais retornada com sucesso.',
+  })
+  @ApiResponse({ status: 404, description: 'Nenhuma nota fiscal encontrado.' })
   getPlanoPorId(@Param('mes') mes: number | undefined, @Param('ano') ano: number, @Param('top') top:number) {
     return this.notasFiscaisService.buscarRankingCLientes(mes,ano, top);
   }
 
   @Get('xml/baixar/:id')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Baixa uma nota fiscal como XML a partir de um ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'XML baixado com sucesso',
+  })
+  @ApiResponse({ status: 404, description: 'Nenhum nota fiscal encontrada.' })
   criarPlano(@Param('id') id: string) {
     return this.notasFiscaisService.baixarXML(id);
   }
 
   @Get('xml/gerar/:id')
+  @ApiTags('Notas-Fiscais')
+  @ApiOperation({ summary: 'Gera uma nota fiscal como XML a partir de um ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'XML gerado com sucesso',
+  })
+  @ApiResponse({ status: 404, description: 'Nenhum nota fiscal encontrada.' })
   atualizarPlano(@Param('id') id: string) {
     return this.notasFiscaisService.gerarXML(id);
   }
 
   @Get('notas/exportar/:id')
+  @ApiOperation({ summary: 'Exporta uma nota fiscal a partir de um ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Nota fiscal exportada com sucesso',
+  })
+  @ApiResponse({ status: 404, description: 'Nenhum nota fiscal encontrada.' })
+  @ApiTags('Notas-Fiscais')
   deletarPlano(@Param('id') id: string,@Param('format') format: ExportNotaFiscalFormat) {
     return this.notasFiscaisService.exportarNotaFiscal(id, format);
   }
